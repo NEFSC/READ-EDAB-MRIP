1080cd76d1aac3fcc1e18f77e2e41c68ce187bfa
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-06-05 18:38:10 -0400

    add species column to rds files, updating targets for new mrip functions

--- _targets.R
+++ _targets.R
@@ -8,6 +8,7 @@ library(targets)
 # library(tarchetypes) # Load other packages as needed.
 
 # remotes::install_github("NEFSC/READ-EDAB-NEesp2")
+devtools::load_all("../READ-EDAB-NEesp2") # Load the package with custom functions.
 
 # Run the R scripts in the R/ folder with your custom functions:
 # tar_source()
@@ -22,26 +23,29 @@ list(
   #### TODO: reformat this to work with function updates
   #### TODO: add code to download recent year data
 
+  #### watch files
 
+  ## TODO: flatten these into a single list, add species as a parameter of create_rec_trips to filter files
+  #### or create another way to map the files by species
 
+  # targets::tar_target(mrip_trips,
+  #                     # list.files(
+  #                     #   path = here::here("inputs/mrip_directed_trips"),
+  #                     #   pattern = glob2rx("mrip*.csv"),
+  #                     #   full.names = TRUE
+  #                     # ),
+  #                     purrr::map(list.dirs(path = here::here("inputs"),
+  #                                          recursive = FALSE),
+  #                                ~list.files(.x,
+  #                                            pattern =  "[0-9].Rds",
+  #                                            full.names = TRUE,
+  #                                            recursive = TRUE,
+  #                                            include.dirs = TRUE)),
+  #                     format = "file"
+  # ),
 
-  #### read in data
-  targets::tar_target(mrip_trips,
-                      # list.files(
-                      #   path = here::here("inputs/mrip_directed_trips"),
-                      #   pattern = glob2rx("mrip*.csv"),
-                      #   full.names = TRUE
-                      # ),
-                      purrr::map(list.dirs(path = here::here("inputs"),
-                                           recursive = FALSE),
-                                 ~list.files(.x,
-                                             pattern =  "[0-9].Rds",
-                                             full.names = TRUE,
-                                             recursive = TRUE,
-                                             include.dirs = TRUE)),
-                      format = "file"
-  ),
-  targets::tar_target(mrip_landing,
+  # it might be better to read in the files based on species name?
+  targets::tar_target(mrip_landing_files,
                       list.files(
                         path = here::here("inputs"),
                         pattern = glob2rx("catch_all_*.Rds"),
@@ -49,7 +53,7 @@ list(
                       ),
                       format = "file"
   ),
-  targets::tar_target(mrip_catch,
+  targets::tar_target(mrip_catch_files,
                       list.files(
                         path = here::here("inputs"),
                         pattern = glob2rx("catch_landings_*.Rds"),
@@ -58,32 +62,49 @@ list(
                       format = "file"
   ),
 
-  #### calculate time series
+  #### read in catch data
   targets::tar_target(
-    rec_trips,
-    NEesp2::create_rec_trips(files = mrip_trips)
+    mrip_catch,
+    purrr::map(mrip_catch_files,
+               ~readRDS(.x))
   ),
   targets::tar_target(
-    total_rec_landings,
-    NEesp2::create_total_rec_landings(mrip_landing |>
-                                        read.csv(
-                                          skip = 46, # of rows you want to ignore
-                                          na.strings = "."
-                                        ) |>
-                                        janitor::clean_names(case = "all_caps"))
+    mrip_landing,
+    purrr::map(mrip_landing_files,
+               ~readRDS(.x))
   ),
 
+  #### calculate time series
+
+  ## TODO: this one is broken
+  # targets::tar_target(
+  #   rec_trips,
+  #   NEesp2::create_rec_trips(files = mrip_trips)
+  # ),
+
+  targets::tar_target(mrip_landing_indicator,
+                      purrr::map(mrip_landing,
+                                 ~create_total_rec_catch(data = .x$data,
+                                                    # species = species,
+                                                    var_name = "landings"))),
+  targets::tar_target(mrip_catch_indicator,
+                      purrr::map(mrip_catch,
+                                 ~create_total_rec_catch(data = .x[[1]],
+                                                         # species = species,
+                                                         var_name = "catch")))#,
+
+
   #### save data
-  targets::tar_target(
-    save_rec_trips,
-    write.csv(rec_trips,
-              file = here::here("outputs", paste0("mrip_trips_", Sys.Date(), ".csv")))
-  ),
-  targets::tar_target(
-    save_rec_landings,
-    write.csv(total_rec_landings,
-              file = here::here("outputs", paste0("mrip_landings_", Sys.Date(), ".csv")))
-  )
+#   targets::tar_target(
+#     save_rec_trips,
+#     write.csv(rec_trips,
+#               file = here::here("outputs", paste0("mrip_trips_", Sys.Date(), ".csv")))
+#   ),
+#   targets::tar_target(
+#     save_rec_landings,
+#     write.csv(total_rec_landings,
+#               file = here::here("outputs", paste0("mrip_landings_", Sys.Date(), ".csv")))
+  # )
 )
 
 # targets::tar_visnetwork()
